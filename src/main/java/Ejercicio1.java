71e46aad383b3fadbf18aa55b6771af011ca5b97
dolly lasso <catherinelasso@yahoo.com>
2022-09-14 07:15:51 -0500

    first commit

--- src/main/java/Ejercicio1.java
+++ src/main/java/Ejercicio1.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Catherine
- */
 public class Ejercicio1 {
+    
+    public void main(String[] args){
+        
+        System.out.println("hola estoy escribiendo codigo para subirlo a git");
+    }
     
 }
---
+++ src/main/java/Ejercicio2.java
@@ -0,0 +1,7 @@
+public class Ejercicio2 {
+     public void main(String[] args){
+        
+        System.out.println("EJERCICIO2");
+    }
+    
+}
